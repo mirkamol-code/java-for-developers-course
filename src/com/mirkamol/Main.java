d52fa5f0b7beb0d66aa334879116b9dfc79f71e0
Mirkamol <mirkamol@Mirkamols-MacBook-Pro.local>
2026-01-14 22:36:52 +0500

    feat(Main): Exercise Write a method that calculates the rental yield of a property. The method should take in the purchase price and the monthly rent. The method should return the rental yield as a percentage.

--- src/com/mirkamol/Main.java
+++ src/com/mirkamol/Main.java
@@ -1,68 +1,16 @@
-char grade = 'B';
 
 void main() {
-useNewSwitchExpresion();
+    int monthlyRentIncome = 1_000;
+    int originalPriceOfProperty = 100_000;
+    double annualRentalYield = calculateAnnualRentalYield(monthlyRentIncome, originalPriceOfProperty);
 
+    System.out.println("Rental yield per year: " + annualRentalYield + " %");
 }
-void useNewSwitchExpresion(){
-    String result = switch (grade){
-        case 'A' -> "Excellent";
-        case 'B', 'C' -> "Pass";
-        default -> "Fail";
-    };
-    System.out.println(result);
-}
-
-void useSwitchExpression(){
-    // byte short int char enums String
-    // Byte Short Integer Character
-    switch (grade){
-        case 'A':
-            System.out.println("Excellent");
-            break;
-        case 'B':
-        case 'C':
-            System.out.println("Pass");
-            break;
-        default:
-            System.out.println("Fail");
-    }
+double calculateAnnualRentalYield(int monthlyRent, int originalPriceOfProperty) {
+    double annualRentalIncome = calculateAnnualRentalIncome(monthlyRent);
+    return annualRentalIncome / originalPriceOfProperty * 100;
 }
 
-void useOrLogicalOperatorsInIf(){
-    String gender = "Male";
-
-    if(gender.equalsIgnoreCase("FEMALE") || gender.equalsIgnoreCase("MALE")){
-        System.out.println("Valid Gender");
-    } else {
-        System.out.println("Invalid Gender");
-    }
-}
-void useAndLogicalOperatorsInIf(){
-    int age = 2;
-    boolean isAdult = age >= 16;
-    if(isAdult){
-        System.out.println("is adult :)");
-    } else if (age == 0){
-        System.out.println("baby :)");
-    }else if (age > 0 && age < 4){
-        System.out.println("toddler :)");
-    } else {
-        System.out.println("is not adult :(");
-
-    }
-
-    System.out.println("code outside if");
-}
-
-void useIfstatementWithConditions(){
-    int age = 15;
-    boolean isAdult = age >= 16;
-    if(isAdult){
-        System.out.println("is adult :)");
-    } else {
-        System.out.println("is not adult :(");
-    }
-
-    System.out.println("code outside if");
+private int calculateAnnualRentalIncome(int monthlyRent) {
+    return monthlyRent * 12;
 }
