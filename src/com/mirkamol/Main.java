64401eb08a482d619182e7fa96144b40dbfb3daa
Mirkamol <mirkamol.m.a@gmail.com>
2026-01-15 20:07:23 +0500

    feat(Main): wrote a program that takes input from console and shows to the output console by checking age

--- src/com/mirkamol/Main.java
+++ src/com/mirkamol/Main.java
@@ -1,21 +1,19 @@
 
 void main() {
-    // Working with BigDecimals
-    BigDecimal number = BigDecimal.TEN;
-    System.out.println(number);
-    System.out.println(number.add(BigDecimal.ONE));
-    System.out.println(number.max(BigDecimal.ZERO));
-    System.out.println(number.compareTo(BigDecimal.TEN));
+    // Scanner
+    System.out.println("Hello what is your name?");
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+    System.out.println("Hello " + input);
+    System.out.println("What is your age?");
+    int age = scanner.nextInt();
+
+    if(age < 16){
+        System.out.println("You are a child");
+    }else {
+        System.out.println("You are an adult");
+    }
+
+
 }
 
-void differentiateDoubleAndBigDecimal() {
-    double number1 = 0.2;
-    double number2 = 0.3;
-    double result = number2 - number1; //Probelem: result should be 0.1 but the output shows 0.09999999999999998
-    System.out.println(result); // do not use doubles for operation of money and etc
-
-    BigDecimal numberB1 = new BigDecimal("0.02");
-    BigDecimal numberB2 = new BigDecimal("0.03");
-    BigDecimal resultB = numberB2.subtract(numberB1);
-    System.out.println(resultB); // here it is 0.01 in output
-}
