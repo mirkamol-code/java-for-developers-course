ef5548eb77363fc447fd33fda8629f4bd2eafa69
Mirkamol <mirkamol@Mirkamols-MacBook-Pro.local>
2026-01-15 15:41:02 +0500

    feat(Main): used break and continue keyword

--- src/com/mirkamol/Main.java
+++ src/com/mirkamol/Main.java
@@ -1,15 +1,32 @@
 
 void main() {
-    // Type interface with var keywoard
-    var name = "Jamila";
-    var names = new String[]{"Jamila", "Alex"};
-    var age = 22;
-    var isAdult = false;
-    var balance = 1_000_000.33;
 
-    System.out.println("Name: " + name);
-    System.out.println("Names: " + Arrays.toString(names));
-    System.out.println("Age: " + age);
-    System.out.println("isAdult: " + isAdult);
-    System.out.println("balance: " + balance);
+}
+
+void useContinueKeyword(){
+    var names = new String[]{
+            "Andre",
+            "Jamila",
+            "Ali",
+            "Jones",
+            "Matt"
+    };
+
+    for (String name : names){
+        if(name.startsWith("A")){
+            continue;
+        }
+        System.out.println(name);
+    }
+}
+
+void useBreakKeyword(){
+    var numbers = new int[]{1,2,3,4,5};
+
+    for (int number : numbers){
+        if(number == 3){
+            break;
+        }
+        System.out.println(number);
+    }
 }
