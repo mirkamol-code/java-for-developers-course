975c11ce0a23721c8df484b2f1fb0d535e569c92
Mirkamol <mirkamol.m.a@gmail.com>
2026-01-15 19:45:13 +0500

    fear(Main): added method of differntiatingDoubleAndBigDecimal

--- src/com/mirkamol/Main.java
+++ src/com/mirkamol/Main.java
@@ -1,53 +1,21 @@
 
 void main() {
-
+    // Working with BigDecimals
+    BigDecimal number = BigDecimal.TEN;
+    System.out.println(number);
+    System.out.println(number.add(BigDecimal.ONE));
+    System.out.println(number.max(BigDecimal.ZERO));
+    System.out.println(number.compareTo(BigDecimal.TEN));
 }
 
-void useUsefulStringStaticMethods(){
-    String number = String.valueOf(1);
-    System.out.println(number); // output -> 1 which is not number but string
+void differentiateDoubleAndBigDecimal() {
+    double number1 = 0.2;
+    double number2 = 0.3;
+    double result = number2 - number1; //Probelem: result should be 0.1 but the output shows 0.09999999999999998
+    System.out.println(result); // do not use doubles for operation of money and etc
 
-    String format = String.format("Number %s", number);
-    System.out.println(format);
-
-    String[] names = {"Jamila", "Alex", "Aisha"};
-    String join = String.join(" | ", names);
-    String join2 = String.join(",", names);
-    System.out.println(join);
-    System.out.println(join2);
-}
-
-void compareStrings(){
-    String name1 = "Jamila";
-    String name2 = "Jamila";
-    String name3 = new String("Jamila");
-
-    System.out.println("String equality with ==");
-    System.out.println(name1 == name2);
-    System.out.println(name1 == name3);
-    System.out.println("String equality with .equals()");
-    System.out.println(name1.equals(name2));
-    System.out.println(name1.equals(name3));
-
-    // TIP NEVER USE == WHEN COMPARING STRINGS
-    // USE == TO CHECK 2 VARIABLES POINT TO EXACT SAME OBJECT IN MEMORY
-}
-
-void workWithStringMethods(){
-    String name = "Jamila";
-    System.out.println(name + " Ahmed");
-    System.out.println(name.contains("Ahmed"));
-    System.out.println(name.toUpperCase());
-    System.out.println(name.toLowerCase());
-    System.out.println("  ".isBlank());
-    System.out.println("  ".isEmpty());
-    System.out.println("  Hello    ".trim());
-    System.out.println(name.startsWith("J"));
-    System.out.println(name.substring(1));
-    System.out.println(name.substring(1,4));
-    System.out.println(name.replace(
-            "J",
-            "S"));
-    System.out.println(name.charAt(0));
-    System.out.println(name.charAt(1));
+    BigDecimal numberB1 = new BigDecimal("0.02");
+    BigDecimal numberB2 = new BigDecimal("0.03");
+    BigDecimal resultB = numberB2.subtract(numberB1);
+    System.out.println(resultB); // here it is 0.01 in output
 }
