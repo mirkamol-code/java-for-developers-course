f9766492066af60cdd02f18b442f9bac1117f6c6
Mirkamol <mirkamol.m.a@example.com>
2026-01-15 16:47:21 +0500

    feat(Main): added string methods

--- src/com/mirkamol/Main.java
+++ src/com/mirkamol/Main.java
@@ -3,30 +3,51 @@ void main() {
 
 }
 
-void useContinueKeyword(){
-    var names = new String[]{
-            "Andre",
-            "Jamila",
-            "Ali",
-            "Jones",
-            "Matt"
-    };
+void useUsefulStringStaticMethods(){
+    String number = String.valueOf(1);
+    System.out.println(number); // output -> 1 which is not number but string
 
-    for (String name : names){
-        if(name.startsWith("A")){
-            continue;
-        }
-        System.out.println(name);
-    }
+    String format = String.format("Number %s", number);
+    System.out.println(format);
+
+    String[] names = {"Jamila", "Alex", "Aisha"};
+    String join = String.join(" | ", names);
+    String join2 = String.join(",", names);
+    System.out.println(join);
+    System.out.println(join2);
 }
 
-void useBreakKeyword(){
-    var numbers = new int[]{1,2,3,4,5};
+void compareStrings(){
+    String name1 = "Jamila";
+    String name2 = "Jamila";
+    String name3 = new String("Jamila");
 
-    for (int number : numbers){
-        if(number == 3){
-            break;
-        }
-        System.out.println(number);
-    }
+    System.out.println("String equality with ==");
+    System.out.println(name1 == name2);
+    System.out.println(name1 == name3);
+    System.out.println("String equality with .equals()");
+    System.out.println(name1.equals(name2));
+    System.out.println(name1.equals(name3));
+
+    // TIP NEVER USE == WHEN COMPARING STRINGS
+    // USE == TO CHECK 2 VARIABLES POINT TO EXACT SAME OBJECT IN MEMORY
+}
+
+void workWithStringMethods(){
+    String name = "Jamila";
+    System.out.println(name + " Ahmed");
+    System.out.println(name.contains("Ahmed"));
+    System.out.println(name.toUpperCase());
+    System.out.println(name.toLowerCase());
+    System.out.println("  ".isBlank());
+    System.out.println("  ".isEmpty());
+    System.out.println("  Hello    ".trim());
+    System.out.println(name.startsWith("J"));
+    System.out.println(name.substring(1));
+    System.out.println(name.substring(1,4));
+    System.out.println(name.replace(
+            "J",
+            "S"));
+    System.out.println(name.charAt(0));
+    System.out.println(name.charAt(1));
 }
