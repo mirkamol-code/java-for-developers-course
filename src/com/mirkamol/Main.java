1e57a798c6a9fc931ff14be70a57def26c3cf35b
Mirkamol <mirkamol.m.a@gmail.com>
2026-01-16 09:43:18 +0500

    feat(Main): add method that deals with and throws Exception

--- src/com/mirkamol/Main.java
+++ src/com/mirkamol/Main.java
@@ -1,19 +1,77 @@
 
 void main() {
-    // Scanner
-    System.out.println("Hello what is your name?");
-    Scanner scanner = new Scanner(System.in);
-    String input = scanner.nextLine();
-    System.out.println("Hello " + input);
-    System.out.println("What is your age?");
-    int age = scanner.nextInt();
-
-    if(age < 16){
-        System.out.println("You are a child");
-    }else {
-        System.out.println("You are an adult");
+    // Exception
+    try {
+        annha();
+    } catch (Exception e) {
+        e.printStackTrace();
     }
+}
+static void annha() throws Exception {
+    jamila();
+}
+static void jamila() throws Exception {
+      john();
+}
 
+static void john() throws Exception {
+        divide(10, 0);
+}
 
+static double divide(int a, int b) {
+    if (b == 0) {
+        throw new IllegalArgumentException("Cannot divide by zero");
+    }
+    return (double) a / b;
+}
+
+void catchUnknownException() {
+    try {
+        int number = Integer.parseInt("1");
+        System.out.println(number);
+
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(10 / i);
+        }
+        System.out.println("end");
+
+    } catch (Exception e) {
+        System.out.println(e.getMessage());
+    } finally {
+        // this is mainly used for clean up purposes
+        System.out.println("Finally always runs");
+    }
+}
+
+void responseToMultipleExceptionInASingleTryCatch() {
+    try {
+        int number = Integer.parseInt("1");
+        System.out.println(number);
+
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(10 / i);
+        }
+        System.out.println("end");
+
+    } catch (NumberFormatException | ArithmeticException e) {
+        System.out.println(e.getMessage());
+    }
+}
+
+void responseToExceptionsUsingMultipleTryCatch() {
+    try {
+        int number = Integer.parseInt("1");
+        System.out.println(number);
+
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(10 / i);
+        }
+        System.out.println("end");
+
+    } catch (NumberFormatException e) {
+        System.out.println("Failed to parse 1x");
+    } catch (ArithmeticException e) {
+        System.out.println("Cannot divide by zero");
+    }
 }
 
